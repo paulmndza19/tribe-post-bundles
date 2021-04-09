8e6781630226d06f6d7d085d290de8da8c45f26c
John Paul Mendoza <paulmndza19@gmail.com>
2021-04-10 01:15:02 +0800

    Add test for insufficient number of images

--- tests/formats/images_test.rb
+++ tests/formats/images_test.rb
@@ -2,15 +2,20 @@ require 'minitest/autorun'
 require_relative '../../formats/images'
 
 describe Images do
-  before do
+  it 'returns a hash with audio details' do
     no_of_images = 10
-    @image_tally = Images.compute(no_of_images)
+    image_tally = Images.compute(no_of_images)
+
+    assert_equal '10 IMG $800.0', image_tally[:total]
+    assert_equal '1 x 10 $800.0', image_tally[:breakdown][0]
+
+    assert_equal 1, image_tally[:breakdown].length
   end
 
-  it 'returns a hash with audio details' do
-    assert_equal '10 IMG $800.0', @image_tally[:total]
-    assert_equal '1 x 10 $800.0', @image_tally[:breakdown][0]
+  it 'raises an error when no_of_images is less than minimum' do
+    no_of_images = 2
 
-    assert_equal 1, @image_tally[:breakdown].length
+    error = assert_raises(RuntimeError) { Images.compute(no_of_images) }
+    assert_equal 'Insufficient number of images', error.message
   end
 end
