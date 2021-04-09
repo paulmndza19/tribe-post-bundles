2237282df01a0e2aa10db1c8f4bf6a39e9ee23e4
John Paul Mendoza <paulmndza19@gmail.com>
2021-04-09 14:25:43 +0800

    Create test for submission class

---
+++ submission.rb
@@ -0,0 +1,56 @@
+require_relative "formats/images"
+require_relative "formats/audio"
+require_relative "formats/video"
+
+class Submission
+  def initialize(submission_formats)
+    @submission_formats = submission_formats
+    @tally = ""
+  end
+
+  def process_tally
+    images_tally = {}
+    audio_tally = {}
+    video_tally = {}
+
+    @submission_formats.each do |format|
+      if format.include? 'IMG'
+        images = format.split(' ')
+        no_of_images = images.first.to_i
+        images_tally = Images.compute(no_of_images)
+      elsif format.include? 'Flac'
+        audio = format.split(' ')
+        no_of_audio = audio.first.to_i
+        audio_tally = Audio.compute(no_of_audio)
+      elsif format.include? 'VID'
+        video = format.split(' ')
+        no_of_video = video.first.to_i
+        video_tally = Video.compute(no_of_video)
+      end
+    end
+
+    build_tally(images_tally)
+    build_tally(audio_tally)
+    build_tally(video_tally)
+
+    @tally
+  end
+
+  private
+
+  def print_results(images_tally, audio_tally, video_tally)
+    print_tally images_tally
+    print_tally audio_tally
+    print_tally video_tally
+  end
+
+  def build_tally(tally_json)
+    return if tally_json.empty?
+
+    @tally+="#{tally_json[:total]}\n"
+
+    tally_json[:breakdown].each do |details|
+      @tally+="\t#{details}\n"
+    end
+  end
+end
---
+++ tests/submission_test.rb
@@ -0,0 +1,22 @@
+require 'minitest/autorun'
+require_relative '../submission'
+
+describe Submission do
+  before do
+    @submission_formats = ['10 IMG', '15 Flac', '13 VID']
+    @submission = Submission.new(@submission_formats)
+  end
+
+  it 'returns a tally' do
+    tally = @submission.process_tally
+
+    assert tally.include? '10 IMG $800.0'
+    assert tally.include? '1 x 10 $800.0'
+    assert tally.include? '15 FLAC $1957.5'
+    assert tally.include? '1 x 9 $1147.5'
+    assert tally.include? '1 x 6 $810.0'
+    assert tally.include? '13 VID $2370.0'
+    assert tally.include? '2 x 5 $900.0'
+    assert tally.include? '1 x 3 $570.0'
+  end
+end
