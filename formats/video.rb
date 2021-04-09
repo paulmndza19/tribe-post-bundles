5dd8d1f81bf4c63181a7b2b1d97b336c720ddabc
John Paul Mendoza <paulmndza19@gmail.com>
2021-04-10 00:56:19 +0800

    Improve login to catch more possible scenarios

--- formats/video.rb
+++ formats/video.rb
@@ -21,28 +21,45 @@ module Video
 
       remainder = video_left%bundle_key
 
-      # check if remainder can still be bundled or if the remainder is the sum of any 2 available bundles
-      can_still_be_bundled = bundle_keys.include?(remainder) || (bundle_keys - [bundle_key]).sum == remainder
-      # also check if there is no remaining VID
-      no_available_video = remainder == 0
+      addends = bundle_keys - [bundle_key]
+      sum_of_any_two_bundle_option = addends.sum
+      sum_of_bundle_keys = bundle_keys.sum
 
-      if can_still_be_bundled || no_available_video
-        number_of_bundles_in_quantity = video_left/bundle_key
-
-        price = number_of_bundles_in_quantity * BUNDLE_PRICES[bundle_key]
-
-        total+=price
-
-        price_breakdown << "#{number_of_bundles_in_quantity} x #{bundle_key} $#{BUNDLE_PRICES[bundle_key]}"
-
-        video_left = remainder
+      if (video_left % sum_of_any_two_bundle_option).zero?
+        return build_breakdown(addends, no_of_video, sum_of_any_two_bundle_option)
+        break
       end
+
+      if (video_left % sum_of_bundle_keys).zero?
+        return build_breakdown(bundle_keys, no_of_video, sum_of_bundle_keys)
+        break
+      end
+
+      number_of_bundles_in_quantity = video_left/bundle_key
+      price = number_of_bundles_in_quantity * BUNDLE_PRICES[bundle_key]
+      total+=price
+      price_breakdown << "#{number_of_bundles_in_quantity} x #{bundle_key} $#{BUNDLE_PRICES[bundle_key]}"
+      video_left = remainder
     end
 
     breakdown(no_of_video, total, price_breakdown)
   end
 
   private
+
+  def self.build_breakdown(addends, no_of_video, divisor)
+    total = 0
+    price_breakdown = []
+    quotient = no_of_video/divisor
+
+    addends.each do |addend|
+      price = quotient * BUNDLE_PRICES[addend]
+      total+=price
+      price_breakdown << "#{quotient} x #{addend} $#{BUNDLE_PRICES[addend]}"
+    end
+
+    breakdown(no_of_video, total, price_breakdown)
+  end
 
   def self.valid?(no_of_video)
     if no_of_video < MINIMUM
@@ -55,5 +72,10 @@ module Video
       total: "#{no_of_video} VID $#{total}",
       breakdown: price_breakdown
     }
+  end
+
+  def self.divisible_by_3_or_5(number)
+    digits = number.to_s.chars.map(&:to_i)
+    (digits.sum % 3).zero? || (number % 5).zero?
   end
 end
